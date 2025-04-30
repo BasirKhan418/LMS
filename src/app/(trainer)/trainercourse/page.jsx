6a6726dbc177b56da1bc7e33d0574560cdcf8a2d
Basir Khan <khanbasir5555@gmail.com>
2025-04-30 14:01:31 +0530

    course page added

---
+++ src/app/(trainer)/trainercourse/page.jsx
@@ -0,0 +1,68 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import SessionDetected from '@/utilities/Auth/SessionDetected'
+import { TrainerValidatesFunc } from '../../../../functions/trainerauthfunc'
+import HomePageSkl from '@/utilities/skeleton/HomePageSkl'
+import { Toaster,toast } from 'sonner'
+import { CourseData } from '../../../../functions/Coursedata'
+import Link from 'next/link'
+import Card from '@/utilities/Course/Card'
+import { useRouter } from 'next/navigation'
+const Page = () => {
+    const router = useRouter();
+    const [data,setData] = useState(null)
+    const [coursesData,setCoursesData] = useState(null)
+    const [loading,setLoading] = useState(false)
+    const [isansession,setisansession]=useState(false);
+    //fetching all courses
+    const fetchallCourse = async(batch)=>{
+    setLoading(true)
+     let data = await CourseData();
+     console.log("course data from trainer",data)
+     setLoading(false)  
+     const filteredData = data.data.filter((item) => batch.includes(item.batch));
+     setCoursesData(filteredData);
+    }
+    //validates correct admin or not
+    const validates = async(token)=>{
+        setLoading(true);
+        let data =  await TrainerValidatesFunc(token);
+        setLoading(false);
+        console.log(data)
+        if(data.success){
+          setData(data.data)
+            fetchallCourse(data.data[0].batches);
+        }
+        else{
+          toast.error(data.message);
+          if(data.ansession){
+            setisansession(true);
+            setTimeout(()=>{
+              router.push("/trainerlogin");
+            },2000)
+          }
+          setTimeout(()=>{
+            router.push("/trainerlogin");
+          },2000)
+        }
+      }
+        useEffect(()=>{
+      validates(localStorage.getItem("dilmsadmintoken"))
+      
+        },[])
+        console.log(coursesData)
+  return (
+    <>
+    <Toaster position='top-center' expand={false}/>
+        {isansession&&<SessionDetected/>}
+   { loading?<HomePageSkl/>:<div className='flex flex-wrap'>
+       
+    {coursesData&&coursesData.map((item)=>(
+        <Link href={`/trainercourse/${item._id}`} key={item._id} className='mx-2 my-2'><Card title={item.title} description={item.desc} duration={item.duration} validity={"1"} progress={20} img={item.img} skills={item.skills} isadmin={true} isBestseller={item.coursetype=="live"?false:true} course={{coursetype:item.coursetype}}/></Link>
+      ))}
+    </div>}
+    </>
+  )
+}
+
+export default Page
--- src/utilities/Trainer/TrainerSidebar.jsx
+++ src/utilities/Trainer/TrainerSidebar.jsx
@@ -76,7 +76,7 @@ export default function TrainerSidebar({children}) {
           </Link>
          
           <Link
-            href="/admincourse"
+            href="/trainercourse"
             className="flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium hover:bg-muted"
             prefetch={false}
           >
